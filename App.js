7269ab7ce8c2645836e755d8c810b7a774ed3da8
Pasi Karpinmaa <t1kapa01@students.oamk.fi>
2023-08-29 18:47:06 +0300

    calculator incoming

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enskat, setEnskat] = useState('')
+  const [krona, setKrona] = useState(0)
+  
+  const calculate = () => {
+    const result = enskat.replace(',', '.') * 11.85
+    setKrona(result)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput 
+      placeholder='Enter enskat'
+      keyboardType='decimal-pad'
+      value={enskat}
+      onChangeText={text => setEnskat(text)}
+      style={styles.field}
+      
+      />
+      <Text style={styles.field}>Krona</Text>
+      <Text style={styles.field}>{krona.toFixed(2)}</Text>
+      <Button title='Kalkuleit' onPress={calculate}/>
     </View>
   );
 }
@@ -14,7 +32,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
   },
+  field: {
+    marginBottom: 10,
+  }
 });
